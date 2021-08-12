f5dad88218e3a8e7d9994530909bd76d0bc09ea4
Graphite2213 <88732099+Graphite2213@users.noreply.github.com>
2021-08-12 09:18:56 +0200

    Add files via upload

--- sha/shamenu.js
+++ sha/shamenu.js
@@ -24,12 +24,30 @@ document.getElementById("buttonsMenu").style.visibility = "visible";
 }
 
 setInterval(function() {
-fetch("http://localhost:3000")
-.then(res => res.json())
-.then(data => {
-  console.log(data);
-})
-.catch(rejected => {
-    console.log(rejected);
-});
+let data = 0;
+
+function reqError(err) {
+  console.log('Fetch Error :-S', err);
+  document.getElementById("shard1").style.color = "blue";
+}
+
+var oReq = new XMLHttpRequest();
+oReq.onreadystatechange = function() {
+data = this.responseText;
+    if (this.readyState == 4 && this.status == 200) {
+if ((new Date().getTime() - data) <= 500) {
+				document.getElementById("shard1").style.color = "green";
+}
+else if (((new Date().getTime() - data) > 500) && ((new Date().getTime() - data) < 200000)) {
+				document.getElementById("shard1").style.color = "yellow";
+}
+else if ((new Date().getTime() - data) >= 200000) {
+			document.getElementById("shard1").style.color = "red";
+}
+    }
+  };
+oReq.onerror = reqError;
+oReq.open('get', `http://${secret.IP}:8321`, true);
+oReq.send();
+
 }, 3000);
