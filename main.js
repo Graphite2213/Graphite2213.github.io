11ea5bf583643ddc31767959c7fcd7641c98c4d8
Graphite <graphite2264@gmail.com>
2021-12-21 18:44:06 +0100

    some changes

--- main.js
+++ main.js
@@ -29,10 +29,9 @@ function runMobileCheck(debugging) {
 
         playErrorSound();
         document.getElementById("phoneError").style.display = "block";
-        document.getElementById("basicinfo").style.display = "none";
-        document.getElementById("hellobox").style.display = "none";
-        document.getElementById("projects").style.display = "none";
-
+        killAllWindows();
+    } else {
+        return true;
     }
 
 }
@@ -43,14 +42,79 @@ function playErrorSound() {
 
 function phoneButtonOk() {
     document.getElementById("phoneError").style.display = "none";
-    document.getElementById("basicinfo").style.display = "block";
-    document.getElementById("hellobox").style.display = "block";
-    document.getElementById("projects").style.display = "block";
+    openAllWindows();
 }
 currentTime();
-runMobileCheck(false);
+if (runMobileCheck(false)) {
+    openAllWindows();
+}
+
+function killAllWindows() {
+    document.getElementById("basicinfo").style.display = "none";
+    document.getElementById("hellobox").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+}
+
+function openAllWindows() {
+    setTimeout(() => document.getElementById("basicinfo").style.display = "block", 500);
+    setTimeout(() => document.getElementById("hellobox").style.display = "block", 200);
+    setTimeout(() => document.getElementById("projects").style.display = "block", 500);
+}
 
 function showprojClick() {
-    document.getElementById("projects").style.visibility = "visible";
+    document.body.style.cursor = "wait";
 
+    setTimeout(() => {
+        document.getElementById("projects").style.visibility = "visible";
+        document.body.style.cursor = "default";
+    }, 500);
+
+}
+let body = document.querySelector("body");
+
+
+// Potential drag functionality
+/*function onDrag({ movementX, movementY, target }) {
+    let getStyle = window.getComputedStyle(target);
+    let left = parseInt(getStyle.left);
+    let top = parseInt(getStyle.top);
+    console.log(getStyle.left);
+    target.parentElement.style.left = `${left + movementX}px`;
+    target.parentElement.style.top = `${top + movementY}px`;
+}
+
+for (let i = 0; i < document.querySelectorAll(".draggableTitle").length; i++) {
+
+    let draggableWindow = body.querySelectorAll(".draggableWindow")[i];
+    let draggableHeader = document.querySelectorAll(".draggableTitle")[i];
+
+
+    draggableHeader.addEventListener("mousedown", () => {
+        draggableWindow.addEventListener("mousemove", onDrag);
+    });
+
+
+
+    document.addEventListener("mouseup", () => {
+        draggableWindow.removeEventListener("mousemove", onDrag);
+    });
+
+}*/
+
+function closeWindow() {
+    //event.target.parentElement.parentElement.parentElement.style.display = "none";
+}
+
+function phoneButtonRedirect() {
+    window.location.href = "mobile.html";
+}
+
+function showprojClickMob() {
+    document.getElementById("box1").style.display = "none";
+    document.getElementById("projects").style.display = "block";
+}
+
+function goBackMobile() {
+    document.getElementById("box1").style.display = "block";
+    document.getElementById("projects").style.display = "none";
 }
