e12d11524663c35f34ff2e99475ef157a539c133
Graphite2213 <88732099+Graphite2213@users.noreply.github.com>
2022-01-22 22:12:10 +0100

    Added chalkboard

--- chalkboard.js
+++ chalkboard.js
@@ -1,13 +1,16 @@
 let color = "#000000";
 let curserver = 'server1';
 let con;
+let user = 'Default';
+let tool = "brush";
+let size = 1;
 let UUID = 0;
 let gridON = true;
 let changes = [];
 
 let socket = new WebSocket("wss://nullsmc.ddns.net:8029");
 
-
+user = prompt("Please enter your preferred username:");
 
 function makeBoard(width, height) {
     let inHTML = "";
@@ -32,19 +35,57 @@ function updateColor(colo) {
 
 document.getElementById("drawing-table").addEventListener("click", (e) => {
     if (!e.target.classList.contains("cell")) return;
-    e.target.style.backgroundColor = color
     let splitAr = e.target.classList.item(1).split("/");
-    matrix[splitAr[0]][splitAr[1]] = color;
-    socket.send(JSON.stringify({ com: "update", materials: [{ material: color, locx: splitAr[0], locy: splitAr[1], server: curserver }] }));
+    switch (tool) {
+        case 'brush':
+            console.log(`${parseInt(splitAr[0] + 1)}/${splitAr[1]}`);
+            switch (size) {
+                case 1:
+                    e.target.style.backgroundColor = color;
+                    socket.send(JSON.stringify({ com: "update", materials: [{ material: color, locx: splitAr[0], locy: splitAr[1], server: curserver }] }));
+                    break;
+
+                case 2:
+                    e.target.style.backgroundColor = color;
+                    document.getElementsByClassName(`${parseInt(splitAr[0]) + 1}/${splitAr[1]}`)[0].style.backgroundColor = color;
+                    document.getElementsByClassName(`${parseInt(splitAr[0]) - 1}/${splitAr[1]}`)[0].style.backgroundColor = color;
+                    document.getElementsByClassName(`${splitAr[0]}/${parseInt(splitAr[1]) + 1}`)[0].style.backgroundColor = color;
+                    document.getElementsByClassName(`${splitAr[0]}/${parseInt(splitAr[1]) - 1}`)[0].style.backgroundColor = color;
+                    socket.send(JSON.stringify({ com: "update", materials: [{ material: color, locx: splitAr[0], locy: splitAr[1], server: curserver }, { material: color, locx: parseInt(splitAr[0]) + 1, locy: splitAr[1], server: curserver }, { material: color, locx: parseInt(splitAr[0]) - 1, locy: splitAr[1], server: curserver }, { material: color, locx: splitAr[0], locy: parseInt(splitAr[1]) + 1, server: curserver }, { material: color, locx: splitAr[0], locy: parseInt(splitAr[1]) - 1, server: curserver }] }));
+                    break;
+            }
+            break;
+
+        case 'square':
+            drawSquare(splitAr[0], splitAr[1]);
+            break;
+    }
 });
 
 function dragOn(e) {
     if (!e.target.classList.contains("cell")) return;
-
-    e.target.style.backgroundColor = color
     let splitAr = e.target.classList.item(1).split("/");
-    matrix[splitAr[0]][splitAr[1]] = color;
-    changes.unshift({ com: "update", material: color, locx: splitAr[0], locy: splitAr[1], server: curserver });
+    switch (tool) {
+        case 'brush':
+            console.log(size);
+            switch (size) {
+                case 1:
+                    e.target.style.backgroundColor = color
+                    changes.unshift({ com: "update", material: color, locx: splitAr[0], locy: splitAr[1], server: curserver });
+                    break;
+
+                case 2:
+                    e.target.style.backgroundColor = color;
+                    document.getElementsByClassName(`${parseInt(splitAr[0]) + 1}/${splitAr[1]}`)[0].style.backgroundColor = color;
+                    document.getElementsByClassName(`${parseInt(splitAr[0]) - 1}/${splitAr[1]}`)[0].style.backgroundColor = color;
+                    document.getElementsByClassName(`${splitAr[0]}/${parseInt(splitAr[1]) + 1}`)[0].style.backgroundColor = color;
+                    document.getElementsByClassName(`${splitAr[0]}/${parseInt(splitAr[1]) - 1}`)[0].style.backgroundColor = color;
+                    changes.unshift({ material: color, locx: splitAr[0], locy: splitAr[1], server: curserver }, { material: color, locx: `${parseInt(splitAr[0]) + 1}`, locy: splitAr[1], server: curserver }, { material: color, locx: `${parseInt(splitAr[0]) - 1}`, locy: splitAr[1], server: curserver }, { material: color, locx: splitAr[0], locy: `${parseInt(splitAr[1]) + 1}`, server: curserver }, { material: color, locx: splitAr[0], locy: `${parseInt(splitAr[1]) - 1}`, server: curserver });
+                    break;
+            }
+
+            break;
+    }
 }
 
 document.getElementById("drawing-table").addEventListener("mousedown", () => {
@@ -52,13 +93,22 @@ document.getElementById("drawing-table").addEventListener("mousedown", () => {
 });
 document.addEventListener("mouseup", () => {
     document.getElementById("drawing-table").removeEventListener("mousemove", dragOn);
-    socket.send(JSON.stringify({ com: "update", materials: changes }));
+    const unique = Array.from(new Set(changes.map(a => `${a.locx}/${a.locy}`)))
+        .map(id => {
+            return changes.find(a => `${a.locx}/${a.locy}` === id)
+        })
+    socket.send(JSON.stringify({ com: "update", materials: unique }));
     changes = [];
 });
 
 socket.onopen = function(e) {
     socket.send(JSON.stringify({ com: "init", server: curserver }));
 };
+
+function changeServers() {
+    curserver = document.getElementById("serverpicker").value;
+    socket.send(JSON.stringify({ com: "init", server: curserver }));
+}
 
 socket.onclose = function(e) {
     alert("Connection to the server has been lost, please refresh your web page");
@@ -71,6 +121,10 @@ socket.onmessage = function(e) {
         con = e.connection;
         matrix = e.matrix;
         makeBoard(190, 40);
+    } else if (e.msg != undefined) {
+        if (e.server == curserver) {
+            addMessage(e.msg, e.user);
+        }
     } else {
         if (e.server == curserver) {
             for (dat of e.materials) {
@@ -79,10 +133,6 @@ socket.onmessage = function(e) {
         }
     }
 };
-
-function swapServers(server) {
-    socket.send(JSON.stringify({ id: UUID, prevserv: curserv, newserv: server, con: con, com: "serverswap" }));
-}
 
 
 function showGrid() {
@@ -107,12 +157,45 @@ function showGrid() {
     }
 }
 
-function addBorder(elem) {
-    document.getElementById("tGrid").style.border = "2px solid rgb(17, 78, 199)";
+function switchTO(item, elem) {
+    tool = item;
+    elem.style.backgroundColor = "rgba(48, 96, 191, 0.5)";
+    elem.style.border = "2px solid rgb(17, 78, 199)";
 }
 
-function removeBorder(elem) {
-    if (!gridON) {
-        document.getElementById("tGrid").style.border = "2px solid transparent";
+function drawSquare(locx, locy) {
+    console.log(locx, locy)
+    switch (size) {
+        case 1:
+            document.getElementsByClassName(`${locx}/${locy}`)[0].style.backgroundColor = color;
+            document.getElementsByClassName(`${locx - 1}/${locy}`)[0].style.backgroundColor = color;
+            document.getElementsByClassName(`${locx}/${locy - 1}`)[0].style.backgroundColor = color;
+            document.getElementsByClassName(`${locx - 1}/${locy - 1}`)[0].style.backgroundColor = color;
+            socket.send(JSON.stringify({ com: "update", materials: [{ material: color, locx: locx, locy: locy, server: curserver }, { material: color, locx: locx - 1, locy: locy, server: curserver }, { material: color, locx: locx, locy: locy - 1, server: curserver }, { material: color, locx: locx - 1, locy: locy - 1, server: curserver }] }));
+            break;
     }
+}
+
+function changeSizes() {
+    size = parseInt(document.getElementById("sizepicker").value);
+}
+
+document.addEventListener('keypress', sendOnKeypress);
+
+function sendOnKeypress(e) {
+    if (e.code == "Enter") sendMessage();
+}
+
+function sendMessage() {
+    let msg = document.getElementById("chatinput").value;
+    if (msg == "") return;
+    addMessage(msg, user);
+    socket.send(JSON.stringify({ com: "message", msg: msg, user: user, server: curserver }));
+    document.getElementById("chatinput").value = "";
+}
+
+function addMessage(m, u) {
+    let fustr = `<br>${u} >> ${m}`;
+    let addr = document.getElementById("chatbody").innerHTML.concat(fustr).substring(document.getElementById("chatbody").innerHTML.concat(`${fustr}`).length - 850, document.getElementById("chatbody").innerHTML.concat(fustr).length);
+    document.getElementById("chatbody").innerHTML = addr;
 }
